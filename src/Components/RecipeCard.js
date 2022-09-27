7f6ca71c7afcb98908be5b83b4536e456881b968
jstabi01 <joeystabi01@gmail.com>
2022-09-27 17:17:19 -0400

    commit work/
    addded reciptes

--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -1,30 +1,49 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 function RecipeCard(props) {
-    return (
-        <div class="row" style={{margin: "10px"}}>
-        <Card style={{ width: '20rem', padding: '10px', backgroundColor: "whitesmoke", fontFamily: "Courier New"}}>
-        <Card.Title style={{float: "center", textAlign: "center", fontWeight: "800", color: "white"}}>{props.recipe}</Card.Title>
-        <Card.Body style={{fontSize: ".75em", padding: "5px"}}>
-            <Tabs defaultActiveKey="home">
-                <Tab style={{color: "white", fontWeight: "600"}} eventKey="ingredients" title="Ingredients">
-                    {props.ingredients}
-                </Tab>
-                <Tab eventKey="home" title="≡">
-                    <Card.Img variant="top" src={props.src}/>
-                </Tab>
-                <Tab style={{color: "white", fontWeight: "600"}} eventKey="directions" title="Directions">
-                    {props.directions}
-                </Tab>
-            </Tabs>
-          
-        </Card.Body>
-      </Card>
-      </div>
-    )
+	return (
+		<div class="row" style={{ margin: "10px" }}>
+			<Card
+				id="mobileSize"
+				style={{
+					padding: "10px",
+					backgroundColor: "whitesmoke",
+					fontFamily: "Courier New",
+				}}>
+				<Card.Title
+					style={{
+						float: "center",
+						textAlign: "center",
+						fontWeight: "800",
+						color: "white",
+					}}>
+					{props.recipe}
+				</Card.Title>
+				<Card.Body style={{ fontSize: ".75em", padding: "5px" }}>
+					<Tabs defaultActiveKey="home">
+						<Tab
+							style={{ color: "white", fontWeight: "600" }}
+							eventKey="ingredients"
+							title="Ingredients">
+							{props.ingredients}
+						</Tab>
+						<Tab eventKey="home" title="≡">
+							<Card.Img variant="top" src={props.src} />
+						</Tab>
+						<Tab
+							style={{ color: "white", fontWeight: "600" }}
+							eventKey="directions"
+							title="Directions">
+							{props.directions}
+						</Tab>
+					</Tabs>
+				</Card.Body>
+			</Card>
+		</div>
+	);
 }
 
 export default RecipeCard;
